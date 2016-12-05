5b136c384f006086a2dce4d42001d1cd45ffd6e3
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2016-12-05 18:35:14 +0100

    Improve dump_file

--- dump_file.cpp
+++ dump_file.cpp
@@ -4,11 +4,10 @@
 
 #include "file_stream_impl.h"
 
-block_size_t print_header(int fd, size_t i, bool head) {
-	block_header header;
+ssize_t _read(int fd, void * buf, size_t size) {
 	ssize_t rt = 0;
-	while (rt < sizeof header) {
-		ssize_t r = ::read(fd, &header, sizeof header);
+	while (rt < size) {
+		ssize_t r = ::read(fd, buf, size);
 		if (r < 0) {
 			perror("read");
 			exit(1);
@@ -18,13 +17,23 @@ block_size_t print_header(int fd, size_t i, bool head) {
 		}
 		rt += r;
 	}
-	ssize_t off = ::lseek(fd, 0, SEEK_CUR) - sizeof header;
+	return rt;
+}
+
+void print_header(size_t i, const block_header & header, bool head, ssize_t off) {
 	std::cout << "Header " << i << " (" << (head? "head": "tail") << ") @ " << off << ":\n"
 			  << "\tLogical offset: " << header.logical_offset << "\n"
 			  << "\tPhysical size: " << header.physical_size << "\n"
 			  << "\tLogical size: " << header.logical_size << "\n"
 			  << "\n";
-	return header.physical_size;
+}
+
+block_header read_and_print_header(int fd, size_t i, bool head) {
+	ssize_t off = ::lseek(fd, 0, SEEK_CUR);
+	block_header header;
+	_read(fd, &header, sizeof header);
+	print_header(i, header, head, off);
+	return header;
 }
 
 void dump_file(const char * fname) {
@@ -40,25 +49,33 @@ void dump_file(const char * fname) {
 
 	ssize_t off = 0;
 
-	block_header header;
-	size_t header_size = sizeof header;
+	block_header h1, h2;
+	size_t header_size = sizeof(block_header);
 
-	for (size_t i = 0; true; i++) {
-		if (i != 0) {
-			print_header(fd, i - 1, false);
-			off += header_size;
+	file_size_t logical_offset = 0;
+
+	for (size_t i = 0; off != size; i++) {
+		h1 = read_and_print_header(fd, i, true);
+		if (h1.logical_offset != logical_offset) {
+			std::cerr << "Wrong logical offset!\n";
+			exit(1);
 		}
-		if (off == size) break;
 
-		auto s = print_header(fd, i, true);
-		off += header_size;
-
-		ssize_t seek_off = s - 2 * header_size;
-		if (off + seek_off >= size) {
+		off += h1.physical_size - header_size;
+		if (off >= size) {
 			std::cerr << "Trying to seek beyond file!\n";
 			exit(1);
 		}
-		off = ::lseek(fd, seek_off, SEEK_CUR);
+		::lseek(fd, off, SEEK_SET);
+		h2 = read_and_print_header(fd, i, false);
+		off += header_size;
+
+		if (memcmp(&h1, &h2, header_size)) {
+			std::cerr << "Header contents differs!\n";
+			exit(1);
+		}
+
+		logical_offset += h1.logical_size;
 	}
 
 	::close(fd);
