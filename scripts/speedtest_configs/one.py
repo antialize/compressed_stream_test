93792393971f4f3091456a0635f3ee294c9f7fc7
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2017-08-29 16:06:12 +0200

    Add config with one test only

---
+++ scripts/speedtest_configs/one.py
@@ -0,0 +1,19 @@
+DEBUG = False
+SHOULD_KILLCACHE = True
+SHOULD_FORMAT = True
+SHOULD_VALIDATE = False
+action_args = range(3) if SHOULD_VALIDATE else range(2)
+
+MB = 2**20
+min_bs = max_bs = MB // 16
+
+# In megabytes
+min_fs = max_fs = 1024
+
+blocksizes = list(exprange(min_bs, max_bs))
+filesizes = list(exprange(min_fs, max_fs))
+
+compression_args = [False]
+readahead_args = [False]
+item_args = [0]
+test_args = [2]
