eb346708ffbdf95542a0852a5073c4644f477baa
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-02-01 15:45:51 +0100

    Add new speedtest_config

---
+++ scripts/speedtest_configs/single_merge_jobs.py
@@ -0,0 +1,23 @@
+DEBUG = False
+SHOULD_KILLCACHE = True
+SHOULD_FORMAT = True
+SHOULD_VALIDATE = False
+action_args = range(3) if SHOULD_VALIDATE else range(2)
+
+TEST_RUNS = 10
+
+MB = 2**20
+min_bs = max_bs = 2 * MB
+
+# In megabytes
+min_fs = max_fs = 2**16
+
+blocksizes = list(exprange(min_bs, max_bs))
+filesizes = list(exprange(min_fs, max_fs))
+
+compression_args = bins
+readahead_args = bins
+item_args = [2]
+test_args = [4]
+merge_params = [64]
+job_args = range(1, 16 + 1)
