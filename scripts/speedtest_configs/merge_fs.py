4b8f9c6ebfabf79ee54316cddca1f89bdc5a5d81
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-02-06 13:23:59 +0100

    Add merge_fs speedtest config

---
+++ scripts/speedtest_configs/merge_fs.py
@@ -0,0 +1,24 @@
+DEBUG = False
+SHOULD_KILLCACHE = True
+SHOULD_FORMAT = True
+SHOULD_VALIDATE = False
+action_args = range(3) if SHOULD_VALIDATE else range(2)
+
+TEST_RUNS = 1000
+
+MB = 2**20
+min_bs = max_bs = 2 * MB
+
+# In megabytes
+min_fs = 2**10
+max_fs = 2**17
+
+blocksizes = list(exprange(min_bs, max_bs))
+filesizes = list(exprange(min_fs, max_fs))
+
+compression_args = [0]
+readahead_args = [0]
+item_args = [2]
+test_args = [4]
+merge_params = [64]
+job_args = [4]
